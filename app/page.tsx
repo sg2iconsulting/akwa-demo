ba5ae4cdd8235c93bc08464226ab15c7d77206b0
aalazhar <alaz.anaz.92@gmail.com>
2024-12-25 10:09:43 +0100

    Routes update

--- app/page.tsx
+++ app/page.tsx
@@ -1,136 +1,3 @@
-// "use client";
-// import React, { useState } from "react";
-// import Link from "next/link";
-
-// const Home = () => {
-//   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-//   const handleMouseEnter = (index: number) => {
-//     setHoveredIndex(index);
-//   };
-
-//   const handleMouseLeave = () => {
-//     setHoveredIndex(null);
-//   };
-
-//   const links = [
-//     {
-//       id: 1,
-//       href: "http://localhost:3000/akwa",
-//       backgroundImage: "url('/speedy/akwa.png')",
-//       title: "Akwa",
-//     },
-//     {
-//       id: 2,
-//       href: "http://localhost:3000/speedy",
-//       backgroundImage: "url('/speedy/navback.jpeg')",
-//       title: "Speedy",
-//     },
-//   ];
-
-//   return (
-//     <div className="w-screen h-screen flex">
-//       {links.map((link, index) => (
-//         <Link
-//           key={link.id}
-//           href={link.href}
-//           className={`relative w-1/2 h-full transition-transform duration-500 ease-in-out ${
-//             hoveredIndex === index ? "scale-110 z-10" : "blur-none"
-//           } ${
-//             hoveredIndex !== null && hoveredIndex !== index ? "blur-sm" : ""
-//           }`}
-//           onMouseEnter={() => handleMouseEnter(index)}
-//           onMouseLeave={handleMouseLeave}
-//         >
-//           <div
-//             style={{
-//               backgroundImage: link.backgroundImage,
-//               backgroundSize: "cover",
-//               backgroundPosition: "center",
-//             }}
-//             className="w-full h-full"
-//           >
-//             <div className="absolute inset-0 bg-black opacity-70"></div>
-
-//             <div className="absolute inset-0 flex items-center justify-center">
-//               <h1 className="text-white text-7xl font-bold">{link.title}</h1>
-//             </div>
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// "use client";
-// import React, { useState } from "react";
-// import Link from "next/link";
-
-// const Home = () => {
-//   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-//   const handleMouseEnter = (index: number) => {
-//     setHoveredIndex(index);
-//   };
-
-//   const handleMouseLeave = () => {
-//     setHoveredIndex(null);
-//   };
-
-//   const links = [
-//     {
-//       id: 1,
-//       href: "http://localhost:3000/akwa",
-//       backgroundImage: "url('/speedy/akwa.png')",
-//       title: "Akwa",
-//     },
-//     {
-//       id: 2,
-//       href: "http://localhost:3000/speedy",
-//       backgroundImage: "url('/speedy/navback.jpeg')",
-//       title: "Speedy",
-//     },
-//   ];
-
-//   return (
-//     <div className="w-screen h-screen flex">
-//       {links.map((link, index) => (
-//         <Link
-//           key={link.id}
-//           href={link.href}
-//           className="relative w-1/2 h-full overflow-hidden"
-//           onMouseEnter={() => handleMouseEnter(index)}
-//           onMouseLeave={handleMouseLeave}
-//         >
-//           {/* Background Container */}
-//           <div
-//             style={{
-//               backgroundImage: link.backgroundImage,
-//               backgroundSize: "cover",
-//               backgroundPosition: "center",
-//             }}
-//             className={`w-full h-full transition-transform duration-500 ease-in-out ${
-//               hoveredIndex === index ? "scale-110" : "scale-100"
-//             }`}
-//           ></div>
-
-//           {/* Black Overlay */}
-//           <div className="absolute inset-0 bg-black opacity-70"></div>
-
-//           {/* Title */}
-//           <div className="absolute inset-0 flex items-center justify-center">
-//             <h1 className="text-white text-7xl font-bold">{link.title}</h1>
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
@@ -149,13 +16,13 @@ const Home = () => {
   const links = [
     {
       id: 1,
-      href: "http://localhost:3000/akwa",
+      href: "/akwa",
       imageSrc: "/speedy/akwa.png",
       title: "Akwa",
     },
     {
       id: 2,
-      href: "http://localhost:3000/speedy",
+      href: "/speedy",
       imageSrc: "/speedy/navback.jpeg",
       title: "Speedy",
     },
