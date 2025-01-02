00da4fd0cd7f5f006692c9886e51a4f8cc381d12
aalazhar <alaz.anaz.92@gmail.com>
2025-01-02 15:53:32 +0100

    updating balloon effect

--- app/maghrebOxygen/page.tsx
+++ app/maghrebOxygen/page.tsx
@@ -1,5 +1,6 @@
-'use client'
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { useInView } from "framer-motion";
 import Navbar from "../components/maghreb/Navbar";
 import Footer from "../components/maghreb/Footer";
 import { motion } from "framer-motion";
@@ -33,19 +34,24 @@ const Home = () => {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+  const lastPostRef = useRef(null);
+  const isLastPostInView = useInView(lastPostRef);
 
   return (
     <section className="flex flex-col gap-6 md:gap-10 lg:gap-20 xl:gap-[100px] 2xl:gap-[160px]">
+      {" "}
       <motion.img
         variants={balloonVariants}
         initial="hidden"
         animate="visible"
         whileHover="float"
-        className="fixed right-5 md:right-9 lg:right-14 xl:right-16 2xl:right-20 top-24 1xl:top-40 2xl:top-48 w-[50px] md:w-[80px] lg:w-[110px] xl:w-[140px] 2xl:w-[170px] z-[2000] cursor-pointer"
+        className={`fixed right-5 md:right-10 xl:right-20  top-24 1xl:top-40 2xl:top-48 w-[35px] md:w-[60px] lg:w-[90px] xl:w-[120px] 2xl:w-[150px] z-[2000] cursor-pointer transition-opacity duration-500 ease-in-out ${
+          isLastPostInView ? "opacity-0 pointer-events-none" : "opacity-100"
+        }`}
         src="/maghreb/Balloon.png"
         alt="Cute Balloon"
         onClick={scrollToTop}
-      ></motion.img>
+      />
       <Navbar />
       <ContactAnimation />
       <AboutUsAnimation />
@@ -62,7 +68,10 @@ const Home = () => {
                 plus sécurisée et avec un nouveau design."
         Date={"23 Decembre 2024"}
       />
-      <LastPostsAnimation />
+      {/* Add ref to LastPostsAnimation */}
+      <div ref={lastPostRef}>
+        <LastPostsAnimation />
+      </div>
       <Footer />
     </section>
   );
