eddbae39bea4674d879f6a1e0e065e06b991e4fb
SG2I CONSULTING <sg2i.dev@gmail.com>
2025-01-07 11:08:30 +0100

    correcteur d'orthographe

--- app/components/fastVolt/Solutions.tsx
+++ app/components/fastVolt/Solutions.tsx
@@ -37,7 +37,7 @@ const Solutions = () => {
             <h2
               className={`w-full leading-none text-[20px] md:text-[24px] xl:text-[40px] 2xl:text-[60px] font-black text-black dark:text-white`}
             >
-              Un solution rapide et proche
+              Une solution rapide et proche
             </h2>
             <h2
               className={`w-full leading-none text-[20px] md:text-[24px] xl:text-[40px] 2xl:text-[60px] font-black text-black dark:text-white`}
